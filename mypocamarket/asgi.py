8b0efe46b54a3ae085fb992175c508990f2c3546
GXONE <rhwldnjs1226@gmail.com>
2024-09-10 10:41:30 +0900

    [Init] 프로젝트 설정

---
+++ mypocamarket/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mypocamarket project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypocamarket.settings')
+
+application = get_asgi_application()
